a9934d9c960b6e806118e366f1ad355de6c5971a
aashiqmustak <aashiqmustak5969@gmail.com>
2026-02-10 15:22:02 +0530

    footer

--- src/components/ZarnexFooter.tsx
+++ src/components/ZarnexFooter.tsx
@@ -1,12 +1,11 @@
 import { Linkedin, Twitter, Youtube, Facebook } from "lucide-react";
 
 const ZarnexLogo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="75" viewBox="0 0 75 20" fill="none">
-    <path d="M71.3463 4.96191H68.6133V14.707H71.3463V4.96191Z" fill="currentColor"/>
-    <path d="M52.5129 14.707C51.3844 14.707 50.4551 15.5988 50.4551 16.7649C50.4551 17.9309 51.3844 18.8227 52.5129 18.8227C53.6414 18.8227 54.5707 17.9309 54.5707 16.7649C54.5707 15.5988 53.6744 14.707 52.5129 14.707Z" fill="currentColor"/>
-    <path d="M63.413 4.99408V5.75097C62.68 5.25739 61.813 4.99408 60.7469 4.99408C58.1469 4.99408 56.0469 7.13448 56.0469 9.86662C56.0469 12.5988 58.1469 14.7061 60.7469 14.7061C61.813 14.7061 62.68 14.4098 63.413 13.9492V14.7061H66.1461V4.99316H63.413V4.99408Z" fill="currentColor"/>
-    <path d="M8.19459 11.6085L13.0974 6.34509H8.53955L3.66334 11.6351V0.879883H0V19.0719H3.66334V16.4397L5.69456 14.2892L9.03955 19.0719H13.6497L8.19459 11.6085Z" fill="currentColor"/>
-  </svg>
+  <img
+    src="/zarnex.ai-logo-bw-.png"
+    alt="Zarnex.ai Logo"
+    className="h-15 w-auto"
+  />
 );
 
 const footerLinks = {
